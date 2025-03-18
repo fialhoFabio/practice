b1843578076b6c10132f4b347e6035fb8b11d56a
fialhofabio <fabiom456@gmail.com>
2025-03-18 11:54:41 -0300

    169 Majority element

---
+++ challenges/169_majority_element.ts
@@ -0,0 +1,15 @@
+function majorityElement(nums: number[]): number {
+  nums = nums.sort()
+  let counter = 0, lastNum = 0, i = 0;
+  while (counter < nums.length / 2) {
+    if (lastNum == nums[i]) counter++;
+    else {
+      lastNum = nums[i];
+      counter = 1;
+    }
+    i++;
+  }
+  return lastNum
+}
+
+console.log(majorityElement([2,2,1,3,1,1,4,1,1,5,1,1,6]))

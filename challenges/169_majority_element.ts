aa977d50b5b0e4b91c2ca42b4ddcccb8ff55a739
fialhofabio <fabiom456@gmail.com>
2025-03-18 12:03:48 -0300

    169 Majority element 2

--- challenges/169_majority_element.ts
+++ challenges/169_majority_element.ts
@@ -1,15 +1,20 @@
+// function majorityElement(nums: number[]): number {
+//   nums = nums.sort()
+//   let counter = 0, lastNum = 0, i = 0;
+//   while (counter < nums.length / 2) {
+//     if (lastNum == nums[i]) counter++;
+//     else {
+//       lastNum = nums[i];
+//       counter = 1;
+//     }
+//     i++;
+//   }
+//   return lastNum
+// }
+
+//Obviooo... o elemento do meio do array vai ser sempre o majority
 function majorityElement(nums: number[]): number {
-  nums = nums.sort()
-  let counter = 0, lastNum = 0, i = 0;
-  while (counter < nums.length / 2) {
-    if (lastNum == nums[i]) counter++;
-    else {
-      lastNum = nums[i];
-      counter = 1;
-    }
-    i++;
-  }
-  return lastNum
+  return nums.sort()[Math.floor(nums.length / 2)]
 }
 
 console.log(majorityElement([2,2,1,3,1,1,4,1,1,5,1,1,6]))
